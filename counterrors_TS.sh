9fe0b1b1bc45ebd7df86045519d12cb6e9c5e983
ololobin <51081040+ololobin@users.noreply.github.com>
2019-05-29 17:18:59 +0300

    Update counterrors_TS.sh

--- counterrors_TS.sh
+++ counterrors_TS.sh
@@ -1,18 +1,20 @@
 # !/bin/bash
 #trap "kill 0" EXIT
+MASK='ext*.log'
+ERR="(\sM\s\[)"
+MAXSIZE=204800 #200KB
 cd /path_to_logs #paste the way to logs
-WATCHFILE=$(ls -t ext*.log | head -n 1) #in my case i am watching files with "ext*.log" mask
+WATCHFILE=$(ls -t $MASK | head -n 1) #In my case i am watching files with "ext*.log" mask
 file=errors.txt #put errors.txt in logs before 1st run
-maxsize=204800 #200KB
 while (true)
 do
     actualsize=$(wc -c <"$file")
-    NEWFILE=$(ls -t ext*.log | head -n 1)
+    NEWFILE=$(ls -t $MASK | head -n 1)
     if [ "$NEWFILE" != "$WATCHFILE" ]; then #New log file
-        grep -E "(\sE\s\[)" $WATCHFILE | wc -l >> $file #in my case i am checking " E "
-        if [ $actualsize -ge $maxsize ]; then #if size(errors.txt)>200KB
-            sed -i '1,10000d' $file #cut first 10000 raws
-        fi
+        grep -E $ERR $WATCHFILE | wc -l >> $file #In my case i am checking " E "
+            if [ $actualsize -ge $MAXSIZE ]; then #if size(errors.txt)>200KB
+                sed -i '1,10000d' $file #cut first 10000 raws
+            fi
         WATCHFILE=$NEWFILE #watching new ext*.log
     fi
 sleep 5
